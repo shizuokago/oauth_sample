bbc10d409a26c0a7d7e16675e961984a1e6b578d
secondarykey <secondarykey@gmail.com>
2016-09-19 12:10:10 +0900

    Draft

--- index.php
+++ index.php
@@ -1,4 +1,12 @@
-<?php
 <html>
-
+    <head>
+        <title>OAuth Sample</title>
+    </head>
+    <body>
+        <form action="/request" method="post">
+            <input type="text" name="cid"/>
+            <input type="text" name="csecret"/>
+            <input type="submit"/>
+        </form>
+    </body>
 </html>
